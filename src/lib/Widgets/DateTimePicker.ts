641e4074c6ad1922189cd83b6b163474cdebda69
Jay Tavares <jay@curiovision.com>
2023-07-26 22:26:13 -0400

    Update DateTimePicker required fields

--- src/lib/Widgets/DateTimePicker.ts
+++ src/lib/Widgets/DateTimePicker.ts
@@ -1,12 +1,12 @@
 import { Action } from '../Action'
 
 export type DateTimePicker = {
-/**
- * Lets users input a date, a time, or both a date and a time.
- *
- * Users can input text or use the picker to select dates and times.
- * If users input an invalid date or time, the picker shows an error that prompts users to input the information correctly.
- */
+  /**
+   * Lets users input a date, a time, or both a date and a time.
+   *
+   * Users can input text or use the picker to select dates and times.
+   * If users input an invalid date or time, the picker shows an error that prompts users to input the information correctly.
+   */
   dateTimePicker: DateTimePickerMarkup
 }
 
@@ -21,21 +21,23 @@ type DateTimePickerMarkup = {
    *
    * For example, if users are scheduling an appointment, use a label such as Appointment date or Appointment date and time.
    */
-  label: string
+  label?: string
 
   /**
    * Whether the widget supports inputting a date, a time, or the date and time.
+   *
+   * Defaults to DATE_AND_TIME.
    */
-  type: DateTimePickerType
+  type?: DateTimePickerType
 
   /**
    * The default value displayed in the widget, in milliseconds since Unix epoch time.
    *
    * Specify the value based on the type of picker (DateTimePickerType):
    *
-   * DATE_AND_TIME: a calendar date and time in UTC. For example, to represent January 1, 2023 at 12:00 PM UTC, use 1672574400000.
-   * DATE_ONLY: a calendar date at 00:00:00 UTC. For example, to represent January 1, 2023, use 1672531200000.
-   * TIME_ONLY: a time in UTC. For example, to represent 12:00 PM, use 43200000 (or 12 * 60 * 60 * 1000).
+   *   - DATE_AND_TIME: a calendar date and time in UTC. For example, to represent January 1, 2023 at 12:00 PM UTC, use 1672574400000.
+   *   - DATE_ONLY: a calendar date at 00:00:00 UTC. For example, to represent January 1, 2023, use 1672531200000.
+   *   - TIME_ONLY: a time in UTC. For example, to represent 12:00 PM, use 43200000 (or 12 * 60 * 60 * 1000).
    */
   valueMsEpoch?: number
 
