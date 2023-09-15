6fc383e912b88123ef0a8688ae491b9557266ee5
Jay Tavares <jay@curiovision.com>
2023-09-15 12:03:52 -0400

    Add missing fields to CommonEventObject

--- src/requests/CommonEventObject.ts
+++ src/requests/CommonEventObject.ts
@@ -8,7 +8,6 @@ export type StringInputs = {
     value: string[]
   }
 }
-
 /**
  * Input Parameter for Date Picker widget.
  */
@@ -30,12 +29,10 @@ export type DateTimeInput = {
      * The time selected by the user, in milliseconds since epoch (00:00:00 UTC on 1 January 1970).
      */
     msSinceEpoch: number
-
     /**
      * true if the input date time includes a date; if false only a time is included.
      */
     hasDate: boolean
-
     /**
      * true if the input date time includes a time; if false only a date is included.
      */
@@ -52,7 +49,6 @@ export type TimeInput = {
      * The hour number selected by the user.
      */
     hours: number
-
     /**
      * The minute number selected by the user.
      */
@@ -65,17 +61,36 @@ export type TimeInput = {
  */
 export type CommonEventObject = {
   /**
+   * The user's language and country/region identifier in the format of ISO 639 language code-ISO 3166 country/region code. For example, en-US.
+   */
+  userLocale?: string
+
+  /**
    * Indicates the host app the add-on is active in when the event object is generated. Possible values include the following:
    * GMAIL
    * CALENDAR
    * DRIVE
    */
   hostApp: 'GMAIL' | 'DRIVE' | 'CALENDAR'
-
   /**
    * Indicates where the event originates (`WEB`, `IOS`, or `ANDROID`)
    */
   platform: 'WEB' | 'IOS' | 'ANDROID'
+  /**
+   * The timezone ID and offset. To enable this field, you must set `addOns.common.useLocaleFromApp` to `true` in your add-on's manifest. Your add-on's scope list must also include `https://www.googleapis.com/auth/script.locale`
+   * See https://developers.google.com/workspace/add-ons/how-tos/access-user-locale for more details
+   */
+  timeZone?: {
+    /**
+     * The timezone identifier of the user's timezone. Examples include: America/New_York, Europe/Vienna, and Asia/Seoul.
+     */
+    id?: string
+    /**
+     * The time offset from Coordinated Universal Time (UTC) of the user's timezone, measured in milliseconds.
+     * See https://developers.google.com/workspace/add-ons/how-tos/access-user-locale for more details
+     */
+    offset?: number
+  }
 
   /**
    * A map containing the current values of the widgets in the displayed card. The map keys are the string IDs assigned with each widget, and each value is another wrapper object with a single "" key.
@@ -84,4 +99,8 @@ export type CommonEventObject = {
     string,
     StringInputs | DateInput | DateTimeInput | TimeInput
   >
+  /**
+   * A map containing the current values of the parameters in the URL query string.
+   */
+  parameters?: Record<string, string>
 }
